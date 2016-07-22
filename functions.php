4a2ec0bd824c8498165034fd3ec50cb7b613ee61
Bradford Knowlton <brad@ucanbonline.net>
2016-07-21 20:07:28 -0600

    missing coma in font include

--- functions.php
+++ functions.php
@@ -52,10 +52,10 @@ require_once( 'library/responsive-images.php' );
 
 function google_fonts() {
 	$query_args = array(
-		'family' => 'Open+Sans:400,700|Oswald:700'
+		'family' => 'Open+Sans:400,700|Oswald:700',
 		'subset' => 'latin,latin-ext',
 	);
 	wp_register_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
-            }
+}
             
 add_action('wp_enqueue_scripts', 'google_fonts');
